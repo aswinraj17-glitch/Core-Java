677c4f79306d4cea732a1f687a66a2f1db599db6
Aswin Raj D <aswinraj790417@gmail.com>
2026-02-15 14:26:00 +0530

    Add files via upload

---
+++ Java Basics/Billsplitter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Billsplitter {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int total=sc.nextInt();
+        int service=sc.nextInt();
+        int gst=sc.nextInt();
+        int people=sc.nextInt();
+
+        double servicep=total*service/100.0;
+        double gstp=(total+servicep)*gst/100.0;
+         
+        double totalamount=total+servicep+gstp;
+
+        double split=totalamount/people;
+        System.out.println("Amount Per Person : "+split);
+
+    }
+    
+}
