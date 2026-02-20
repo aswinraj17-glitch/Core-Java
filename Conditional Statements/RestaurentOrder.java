54015d637803ebe256ab9a889428df75fa16ca05
Aswin Raj D <aswinraj790417@gmail.com>
2026-02-20 14:37:53 +0530

    Add files via upload

---
+++ Conditional Statements/RestaurentOrder.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+public class RestaurentOrder {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String orderType =sc.nextLine();
+        String loyaltyStatus =sc.nextLine();
+        double orderValue =sc.nextDouble();
+        sc.nextLine();
+        String timeSlot =sc.nextLine();
+        System.out.println("OrderType : "+orderType);
+        System.out.println("Loyalty Status : "+loyaltyStatus);
+        System.out.println("Order Value : $"+orderValue);
+        System.out.println("Time Slot : "+timeSlot);
+
+        int baseDiscount=0;
+        if(loyaltyStatus.equals("Gold")){
+            baseDiscount=15;
+        }
+        else if(loyaltyStatus.equals("Silver")){
+            baseDiscount=10;
+        }
+        else if(loyaltyStatus.equals("Bronze")){
+            baseDiscount=5;
+        }
+        else if(loyaltyStatus.equals("None")){
+            baseDiscount=0;
+        }
+
+        int timeBased=0;
+        if(timeSlot.equals("Peak")){
+            timeBased=0;
+        }
+        else if(timeSlot.equals("Regular")){
+            timeBased=0;
+        }
+        else if(timeSlot.equals("Late Night")){
+            timeBased=-5;
+        }
+        double serviceFee=0.0;
+        if(orderType.equals("Delivery")){
+            serviceFee=orderValue*0.10;
+        }
+        else if(orderType.equals("Takeout")){
+            serviceFee=orderValue*0.3;
+        }
+        else if(orderType.equals("Dine-In")){
+            serviceFee=orderValue*0;
+        }
+        int totaldiscount=baseDiscount+timeBased;
+        double discountedValue=orderValue*(1-totaldiscount/100);
+        double finalAmount=discountedValue+serviceFee;
+        String KitchenPriority="";
+        if(timeSlot.equals("Peak") && orderType.equals("Delivery")){
+            KitchenPriority="High";
+        }
+        else if(timeSlot.equals("Regular")){
+            KitchenPriority="Medium";
+        }
+        else if(timeSlot.equals("Late-Night")){
+            KitchenPriority="Low";
+        }
+        System.out.println("Base Discount : "+baseDiscount+"%");
+        System.out.println("Time-Based Adjustment : "+timeBased+"%");
+        System.out.println("Service Fee : $"+serviceFee);
+        System.out.println("Final Amount : $"+finalAmount);
+        System.out.println("Kitchen priority : "+KitchenPriority);
+
+
+
+
+        }
+
+    }
+
