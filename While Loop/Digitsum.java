f32f601c9ecf8cd31e2156d37ab6163eef542eb2
Aswin Raj D <aswinraj790417@gmail.com>
2026-03-04 13:22:01 +0530

    Add files via upload

---
+++ While Loop/Digitsum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Digitsum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int sum=0;
+        int digit=0;
+        System.out.print("Number : ");
+        int num=sc.nextInt();
+        do{
+            digit=num%10;
+            sum=sum+digit;
+            num=num/10;
+
+        }while(num>0);
+        System.out.println("Sum of Digits : "+sum);
+
+
+
+    }
+    
+}
