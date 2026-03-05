e47c82196d6151c3a84135f527691df88b33be40
Aswin Raj D <aswinraj790417@gmail.com>
2026-03-05 10:00:10 +0530

    Add files via upload

---
+++ While Loop/lcm.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class lcm {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a;
+        int d=b;
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+            
+        }
+        System.out.println("LCM of "+c+" and "+d+" = "+(c*d/a));
+
+    }
+    
+}
