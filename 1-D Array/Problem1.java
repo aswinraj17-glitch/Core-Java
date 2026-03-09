9d59adae8bda8a427e91ed27b1246e2484faa620
Aswin Raj D <aswinraj790417@gmail.com>
2026-03-09 14:23:45 +0530

    Add files via upload

---
+++ 1-D Array/Problem1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Problem1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double []arr=new double[n];
+        double average=0;
+        double sum=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextDouble();
+            sum+=arr[i];
+            
+        }
+        average=sum/n;
+
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>average){
+                count++;
+            }
+        }
+        double percentage=(double)count/n*100.0;
+        System.out.println("Total Employees : "+n);
+        System.out.println("Average Salary : "+String.format("%.2f", average));
+        System.out.println("Employees Above Average : "+count);
+        System.out.println("Percentage : "+String.format("%.2f", percentage)+"%");
+    }
+
+}
