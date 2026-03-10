bc43204e2b7f3df030d0e042c1c84f7642ff499e
Aswin Raj D <aswinraj790417@gmail.com>
2026-03-10 22:04:46 +0530

    Add files via upload

---
+++ 1-D Array/program11.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class program11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double []quality=new double[n];
+        double []price=new double[n];
+        double []empty=new double[n];
+        for(int i=0;i<n;i++){
+            quality[i]=sc.nextDouble();
+
+        }
+        for(int i=0;i<n;i++){
+            price[i]=sc.nextDouble();
+        }
+        double max=-1;
+        double min=Double.MAX_VALUE;
+        int minindex=0;
+        int maxindex=0;
+        double total=0;
+        for(int i=0;i<n;i++){
+            empty[i] = quality[i] * price[i];
+            total+=empty[i];
+            if(empty[i]>max){
+                max=empty[i];
+                maxindex=i;
+            }
+            if(empty[i]<min){
+                min=empty[i];
+                minindex=i;
+            }
+
+
+           
+        }
+       System.out.println("Number of Assets : "+n);
+       System.out.println("Total Portfolio Value : $"+total);
+       System.out.println("Most Valuable Asset : Asset "+(maxindex+1)+" ($"+String.format("%.2f", max)+")");
+       System.out.println("Least Valuable Asset : Asset "+(minindex+1)+" ($"+String.format("%.2f", min)+")");
+
+    
+
+
+    }
+    
+}
