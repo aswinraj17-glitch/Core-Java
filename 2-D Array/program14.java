879086f26ec305596eba6048f994cfaac9a9fe92
Aswin Raj D <aswinraj790417@gmail.com>
2026-03-19 18:31:53 +0530

    Add files via upload

---
+++ 2-D Array/program14.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class program14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+        int[][] matrix=new int[N][N];
+        int[][] rotated=new int[N][N];
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                rotated[j][N-1-i]=matrix[i][j];
+            }
+        }
+        System.out.println("Original Matrix:");
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Rotated Matrix (90 deg clockwise):");
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                System.out.print(rotated[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
