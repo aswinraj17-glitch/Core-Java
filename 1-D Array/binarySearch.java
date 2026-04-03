e718c126eef2b2a60c563c9d3f6a00fc4eaa49c0
Aswin Raj D <aswinraj790417@gmail.com>
2026-04-03 11:30:09 +0530

    Add files via upload

---
+++ 1-D Array/binarySearch.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+        int key=sc.nextInt();
+        int high=n-1;
+        int low=0;
+        int found=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==key){
+                found=mid;
+                break;
+            }
+            else if(arr[mid]<key){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+
+        }
+        if(found!=-1){
+            System.out.println("Element found at the index : "+found);
+
+        }else{
+            System.out.println("Element not Found");
+        }
+    }
+    
+}
