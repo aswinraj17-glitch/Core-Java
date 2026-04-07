d1a702d3ca2b652da391d642bb4e6ef8072e9674
Aswin Raj D <aswinraj790417@gmail.com>
2026-04-07 14:24:10 +0530

    Add files via upload

---
+++ Strings/problem2.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class problem2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Password Analysis:");
+
+        for (int i = 0; i < n; i++) {
+            String password = sc.nextLine();
+
+            boolean hasUpper = false;
+            boolean hasLower = false;
+            boolean hasDigit = false;
+            boolean hasSpecial = false;
+
+            // Check each character
+            for (int j = 0; j < password.length(); j++) {
+                char ch = password.charAt(j);
+
+                if (Character.isUpperCase(ch))
+                    hasUpper = true;
+                else if (Character.isLowerCase(ch))
+                    hasLower = true;
+                else if (Character.isDigit(ch))
+                    hasDigit = true;
+                else
+                    hasSpecial = true;
+            }
+
+            String result;
+
+            // Strength checking
+            if (hasUpper && hasLower && hasDigit && hasSpecial && password.length() >= 6)
+                result = "Strong";
+            else
+                result = "Weak";
+
+            System.out.println((i + 1) + ". " + password + " - " + result);
+            sc.close();
+        }
+    }
+}
