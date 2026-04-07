293b2b6f10df9241f77ed0d40e4ebd2af21ab51e
Aswin Raj D <aswinraj790417@gmail.com>
2026-04-07 13:57:46 +0530

    Add files via upload

---
+++ Strings/problem1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+        String[] emails = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            emails[i] = sc.nextLine();
+        }
+
+        int validCount = 0;
+        int invalidCount = 0;
+
+        System.out.println("Email Validation Results:");
+
+        for (int i = 0; i < n; i++) {
+            if (isValidEmail(emails[i])) {
+                System.out.println((i + 1) + ". " + emails[i] + " - Valid");
+                validCount++;
+            } else {
+                System.out.println((i + 1) + ". " + emails[i] + " - Invalid");
+                invalidCount++;
+            }
+        }
+
+        System.out.println("Total Valid: " + validCount);
+        System.out.println("Total Invalid: " + invalidCount);
+    }
+
+    public static boolean isValidEmail(String email) {
+        // Basic email validation using regex
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        return email.matches(regex);
+    }
+}
