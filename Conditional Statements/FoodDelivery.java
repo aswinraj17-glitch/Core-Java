439efd39c21b7db7c2218f2d4e0d38606d34cdb9
Aswin Raj D <aswinraj790417@gmail.com>
2026-04-13 22:36:59 +0530

    Update FoodDelivery.java

--- Conditional Statements/FoodDelivery.java
+++ Conditional Statements/FoodDelivery.java
@@ -88,7 +88,7 @@ public class FoodDelivery {
         System.out.println("Estimated Delivery Time: " + estimatedTime + " minutes");
         System.out.println("Service Priority: " + priority);
 
-        sc.close();
+        
         
 
     }
